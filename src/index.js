d4383e2b4345d46fd891b76879bbef84367d0b71
andreygp5 <andreygp555@gmail.com>
2021-01-25 18:05:37 +0300

    feat: add reverse int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    nString = String(n);
+    nString = nString[0] == "-" ? nString.slice(1) : nString;
+    return Number(Array.from(nString).reverse().join(""));
+};
